c8743bfbd94ad2a03b90069d88fe53773cd286ec
ArthurVDB123 <77455010+ArthurVDB123@users.noreply.github.com>
2021-01-28 16:52:03 +0000

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,26 @@
-
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    let mySprite: Sprite = null
+    mySprite.vy = 0
+})
+scene.setBackgroundColor(9)
+let cat = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . f . . . 
+    . . . . . . . . . . . . f f f . 
+    . . . . . . . . . . . . f f 5 f 
+    f f f f f f f f f f f f f f f f 
+    . . . . f f f f f f f f f f . . 
+    . . . . f f f f f f f f f . . . 
+    . . . . f . f . . . f . f . . . 
+    . . . . f . f . . . f . f . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(cat, 100, 0)
+tiles.setTilemap(tilemap`level1`)
+cat.ay = 200
